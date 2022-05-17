10a22f7bd36b337269e8699d5ed279e428c8e506
vivianzhang <jzhang230@usfca.edu>
2022-05-16 23:10:21 -0700

    Add download() function in bitTorrent class.

--- src/main/java/bitTorrent/BitTorrent.java
+++ src/main/java/bitTorrent/BitTorrent.java
@@ -23,22 +23,33 @@ import static utility.BuildIHave.buildIHave;
 public class BitTorrent {
 
     public static boolean timerCancel = false;
+    private final List<String> announces = new ArrayList<>();
+    private final List<byte[]> eachPiece = new ArrayList<>();
+    private final Map<byte[], Socket> clientSockets = new ConcurrentHashMap<>();
+    private final Map<Integer, byte[]> pieceReceived = new ConcurrentHashMap<>();
+    private final Map<String, BitSet> peersHave = new ConcurrentHashMap<>();
+    private final Map<String, Socket> peersSocket = new ConcurrentHashMap<>();
+    private final BitSet iHave;
+    private final RandomAccessFile file;
+    private final Map<String, BEncodedValue> info;
+    private final String mode;
+    private final String peerHostName;
+    private final int peerPort;
+
 
     public BitTorrent(int port, String torrentFilePath, String mode, String peerHostName, int peerPort) throws IOException {
-
-        List<String> announces = new ArrayList<>();
-        List<byte[]> eachPiece = new ArrayList<>();
-        Map<byte[], Socket> clientSockets = new ConcurrentHashMap<>();
-        Map<Integer, byte[]> pieceReceived = new ConcurrentHashMap<>();
-        Map<String, BitSet> peersHave = new ConcurrentHashMap<>();
-        Map<String, Socket> peersSocket = new ConcurrentHashMap<>();
-
-        Map<String, BEncodedValue> info = parseTorrentFile(eachPiece, announces, torrentFilePath);
-        BitSet iHave = new BitSet(eachPiece.size());
+        this.mode = mode;
+        this.peerHostName = peerHostName;
+        this.peerPort = peerPort;
+        info = parseTorrentFile(eachPiece, announces, torrentFilePath);
+        iHave = new BitSet(eachPiece.size());
         Files.createDirectories(Path.of("target", String.valueOf(port)));
-        RandomAccessFile file = new RandomAccessFile(Path.of("target", String.valueOf(port), info.get("name").getString()).toFile(), "rws");
+        file = new RandomAccessFile(Path.of("target", String.valueOf(port), info.get("name").getString()).toFile(), "rws");
         buildIHave(info, eachPiece, iHave, port);
         startService(port, info, iHave, clientSockets);
+    }
+
+    public void download () {
         connectToPeers(peersSocket, pieceReceived, peersHave, info, eachPiece, announces, mode, peerHostName, peerPort);
         sendRequest(iHave, peersHave, peersSocket, info, eachPiece);
         doChecksum(iHave, file, info, pieceReceived, eachPiece, clientSockets);
@@ -49,9 +60,9 @@ public class BitTorrent {
     public static void main(String[] args) {
         try {
             if (args.length > 3) {
-                new BitTorrent(Integer.parseInt(args[0]), args[1], args[2], args[3], Integer.parseInt(args[4]));
+                new BitTorrent(Integer.parseInt(args[0]), args[1], args[2], args[3], Integer.parseInt(args[4])).download();
             } else {
-                new BitTorrent(Integer.parseInt(args[0]), args[1], args[2], null, -1);
+                new BitTorrent(Integer.parseInt(args[0]), args[1], args[2], null, -1).download();
             }
         } catch (IOException e) {
             e.printStackTrace();
